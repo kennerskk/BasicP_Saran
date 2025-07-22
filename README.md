798dbb330be7df905362a6b0d5a401f866fe54cc
Student <Student@Train11-5-PC116.control.ad.sit.kmutt.ac.th>
2025-07-22 15:24:17 +0700

    ส่งงานให้พี่แกปตรวจครับ

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BasicP_Saran
+# Saran 68130500066
---
+++ class2.py
@@ -0,0 +1,68 @@
+# age = 15
+# print(age)
+
+# num = 5 
+# print(type(num))
+
+# num2 = 1.1
+# print(type(num2))
+
+# boolean = True
+# print(type(boolean))
+
+# name = "Saran"
+# print(type(name))
+# print(name)
+
+# age = "14"
+# print(type(age))
+
+# age = int(age)
+# print(type(age))
+
+# num1 = 5
+# print(type(num1))
+
+# num1 = float(num1)
+# print(num1)
+
+# age = 20 
+# print(type(age))
+
+# age = str(age)
+# print(type(age))
+
+# tf = "True"
+# tf = bool(tf)
+# print(type(tf))
+
+# sum = "Sarayut "+"Khuprachamit"
+# print(sum)
+
+# sum = "5==2" != "5==2 "
+# print(sum)
+
+# age = input("Enter ur age")
+# print("Ur age is",age)
+
+# name = input("Input ur name")
+# print("Ur name is",name)
+
+# def foo():
+#     print("heloo world")
+#     if False:
+#         print("true")
+#     else:
+#         print(False)
+
+# foo()
+
+# age = int(input("input ur age"))
+# if age > 18:
+#     print("ur age is above 18")
+# elif age > 10:
+#     print("ur age is above 10")
+
+# else:
+#     print("ur age is below 18")
+
