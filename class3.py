e5f05797ee7c3b64218686920a7daab9d1522c19
kennerskk <149176535+kennerskk@users.noreply.github.com>
2025-07-23 11:16:49 +0700

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,56 @@
+# haverice = True
+# havespoon = True
+# if(haverice):
+#     if(havespoon):
+#         print("Eat")
+        
+
+# คำนวนเกรด ใช้ได้แค่ nested if
+
+# >50 = d
+# >60 = c
+# >70 = b
+# >80 = a 
+
+# score = int(input("input ur score : "))
+# grade = ""
+# if score >= 0:
+#     if score <= 100:
+#         if score < 50:
+#             grade = "f"
+#             if score > 49:
+#                 grade = "d"
+#                 if score > 59:
+#                     grade = "c"
+#                     if score > 69:
+#                         grade = "b"
+#                         if score > 79:
+#                             grade = "a"
+# print("u got ",grade)
+
+# for i in range(1,10,2):
+#     print(i ,end=" ")
+# print(" ")
+# j=1
+# while j<100:
+#     print(j,end=" ") 
+#     j +=2
+#     if j > 10:
+#         break
+
+# while True:
+#     choice = int(input("input 1 for plus,2 for quit : "))
+
+#     if (choice == 1):
+#         num = int(input("amount of number : "))
+#         sumation = 0
+#         for i in range(num):
+#             num1 = int(input(f"input number {i+1} : "))
+#             sumation += num1
+
+#         print("result is",sumation)
+#     if (choice == 2):
+#         print("Goodbye")
+#         break
+
+# 
